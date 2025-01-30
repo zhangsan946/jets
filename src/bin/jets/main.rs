e3b5a0e37e431ffbea4a408bb1c7009a5e10169a
San Zhang <sanzhang@mail.com>
2025-01-30 19:35:18 +0800

    init
    Signed-off-by: San Zhang <sanzhang@mail.com>

---
+++ src/bin/jets/main.rs
@@ -0,0 +1,8 @@
+use jets::Jets;
+
+fn main() { 
+    let jets = Jets {
+        name: "zhangsan".to_string(),
+    };
+    println!("{}", jets.name);
+}
---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+//! [![crates.io](https://img.shields.io/crates/v/jets?logo=rust)](https://crates.io/crates/jets/)
+//! [![docs.rs](https://docs.rs/jets/badge.svg)](https://docs.rs/jets)
+//! 
+//! jets - A Rust rule-based tunnel
+pub struct Jets {
+    pub name: String,
+}
